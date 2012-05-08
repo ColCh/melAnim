6ebfafc933e28fa88bd64cbab34630794055f1d0
Colch <colouredchalkmelky@gmail.com>
2012-05-08 13:06:39 +0400

    Сделал вычисление значений для cubic-bezier в классической функции анимирования

--- anim.js
+++ anim.js
@@ -20,6 +20,15 @@
 
     // для добавления конечных стилей после отрисовки
     var requestAnimFrame = window.requestAnimationFrame;
+
+    // изинги
+    var easings = {
+        'ease':        [0.25, 0.1, 0.25, 1.0], 
+        'linear':      [0.00, 0.0, 1.00, 1.0],
+        'ease-in':     [0.42, 0.0, 1.00, 1.0],
+        'ease-out':    [0.00, 0.0, 0.58, 1.0],
+        'ease-in-out': [0.42, 0.0, 0.58, 1.0]
+    };
 /*-------------------- </begin.js> --------------------*/
 
 /*-------------------- <init.js> --------------------*/
@@ -255,10 +264,80 @@
         });
     };
 
+
+    var mathemated = {};
+
     // превратить cubic-bezier в обычную функцию
-    var mathemate = function(easing){
-       return function(a){return a*a;}; 
+    var mathemate = function(name){
+        if(!mathemated[name]){
+            mathemated[name] = function(progress){
+                var points = easings[name];
+                return calc.apply(this, points.concat(progress));
+            };
+        }
+
+        return mathemated[name];
     };
+
+
+    var calc = function(p1x, p1y, p2x, p2y, progress){
+
+        var res;
+        
+        if(p1x === p1y && p2x === p2y) {
+
+            res = progress;
+
+        } else {
+            
+            var timeX = getTimeX(progress, p1x, p2x);            
+            res = calcBezier(timeX, p1y, p2y);
+
+        }
+
+        return res; 
+    }
+
+
+
+    // вспомогательные ф-и  
+    var getTimeX = function(progr, p1x, p2x){
+                
+        var timeX = progr;
+
+        for(var i = 0; i < 4; ++i) {
+
+            var currentslope = slope(timeX, p1x, p2x);
+
+            if(currentslope == 0.0) {
+                return timeX;
+            }
+
+            var currentx = calcBezier(timeX, p1x, p2x) - progr;
+
+            timeX -= currentx / currentslope;
+        }
+
+        return timeX;
+    
+    };
+    var calcBezier = function(progr, px1, px2) {
+        return (  ( A(px1,px2)*progr + B(px1,px2) )*progr + C(px1)  )*progr;
+    }
+    var slope = function(progr, px1, px2) {
+        return 3.0*A(px1, px2)*progr*progr + 2.0*B(px1, px2)*progr + C(px1);
+    }
+
+    // укоротители
+    var A = function (aA1, aA2){ 
+        return 1.0 - 3.0 * aA2 + 3.0 * aA1;
+    }
+    var B = function (aA1, aA2){ 
+        return 3.0 * aA2 - 6.0 * aA1;
+    }
+    var C = function (aA1){
+        return 3.0 * aA1;
+    }
 /*-------------------- </classic.js> --------------------*/
 
 /*-------------------- <css.js> --------------------*/
