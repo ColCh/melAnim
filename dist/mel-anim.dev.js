7d6b49adedeb0d4765014b1e9feaa2a575c16e19
Colch <colouredchalkmelky@gmail.com>
2012-08-26 22:46:51 +0400

    Добавил шапку

---
+++ dist/mel-anim.dev.js
@@ -0,0 +1,25 @@
+
+/**
+ * @author melky
+ * @license melAnim.js
+ *
+ * Copyright 2012, Maxim Sysoev ( colch@ro.ru | coloured_chalk@mail.ru )
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *    http://www.opensource.org/licenses/mit-license.php
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ * Version: %version.
+ * Build Date: %build_date.
+ * */
+
+(function (window) {
+	"use strict";
+	//#include ../src/variables.js
+	//#include ../src/animate_wrap.js
+	//#include ../src/animate_animations.js
+	//#include ../src/animate_classic.js
+	//#include ../src/normalize.js
+	//#include ../src/css.js
+	//#include ../src/init.js
+})(window);
