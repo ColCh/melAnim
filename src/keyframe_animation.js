ea51667945e4d2b7a9eacb1756f8ed1d06ddf715
Colch <colouredchalkmelky@gmail.com>
2013-01-04 15:09:38 +0400

    Пофиксил непересекающиеся значения

--- src/keyframe_animation.js
+++ src/keyframe_animation.js
@@ -1,3 +1,6 @@
+    //TODO удалить, т.к. директива определена в animate_wrap.js
+    var DEFAULT_EASING = "ease";
+
     /**
      * Конструктор анимаций с ключевыми кадрами
      * отличается от обычной тем, что
@@ -136,7 +139,7 @@
          * возможно установить особое смягчение для свойства
          * При установке смягчения для свойства параметр прогресса игнорируется
          * @param {Function} easing функция смягчения
-         * @param {number=} position прогресс в долях (по умол. 1)
+         * @param {number=} position прогресс в долях (по умол. для всей анимации)
          * @param {string=} property для какого свойства устанавливается (по умол. для всех)
          */
         easing:function (easing, position, property) {
@@ -145,7 +148,7 @@
                 this.specialEasing[property] = easing;
             } else {
                 if (type.undefined(position)) {
-                    position = keyAliases["to"];
+                    this.smoothing = easing;
                 } else {
                     position = normalizeKey(position);
                 }
@@ -288,55 +291,53 @@
          */
         fetch:function (elapsedTime) {
 
-            var easing, timingFunction, i;
+            var i;
             var self = this;
+            var keyframes, fetchedProperties, firstKeyframe, secondKeyframe, from, to, property;
+            var element = self.target;
+            var progr, fractionalTime, offset, scale;
+            var easing, timingFunction;
 
-            var keyframes;
-            var firstKeyframe, secondKeyframe;
-
-            var fetchedProperties;
-
-            var progr = 1e2 * elapsedTime / self.animationTime;
+            //прогресс анимации без учёта итераций в процентах
+            progr = 100 * elapsedTime / self.animationTime;
 
             if (progr > 100) progr = 100;
 
-            // TODO fetching для нескольких элементов?
-
-            // поиск двух ключевых кадров для текущего прогресса
-            // должно выполняться неравенство
-            // для первого кадра :
-            // КЛЮЧ_ПРЕДЫДУЩЕГО <= ТЕКУЩИЙ_ПРОГРЕСС < КЛЮЧ_СЛЕДУЮЩЕГО
-            // для второго :
-            // КЛЮЧ_ПРЕДЫДУЩЕГО < ТЕКУЩИЙ_ПРОГРЕСС <= КЛЮЧ_СЛЕДУЮЩЕГО
             keyframes = self.keyframes;
 
+            timingFunction = self.smoothing || cubicBezierApproximations[ DEFAULT_EASING ];
+
+            // поиск функции смягчения для текущего ключевого кадра
             i = 0;
-            while (i < keyframes.length) {
-
-                // TODO доделать смягчение
-
-                if ((keyframes[i].key < progr || (keyframes[i].key === progr && progr === 100)) && type.function(keyframes[i].easing)) {
-                    timingFunction = keyframes[i].easing;
-                    break;
+            while (i < keyframes.length - 1) {
+                // КЛЮЧ_ПРЕДЫДУЩЕГО <= ПРОГРЕСС < КЛЮЧ_СЛЕДУЮЩЕГО
+                //TODO первое условие с циклом можно заменить бинарным поиском
+                if (keyframes[i].key <= progr && keyframes[i + 1].key > progr) {
+                    if (keyframes[i].easing) {
+                        timingFunction = keyframes[i].easing;
+                        break;
+                    }
                 }
                 i += 1;
             }
 
+            // высчитанные значения свойств
             fetchedProperties = {};
 
-            var firstKeyframe, secondKeyframe;
-            var from, to;
-            var i;
-            var element = self.target;
-            var offset, scale;
-            var property;
-
+            // в intrinsic находятся начальные значения
+            // всех анимируемых свойств
+            // высчитываем значение каждого свойства
             for (property in this.intrinsic) {
 
-                // TODO replace linear to binary search
-                for (i = 0; i < keyframes.length; i++) {
+                // поиск двух ближайших ключевых кадров
+                // для которых задано значение свойства
+                firstKeyframe = keyframes[0];
+                secondKeyframe = keyframes[keyframes.length - 1];
+
+                //TODO было бы неплохо тоже заменить линейный поиск на бинарный
+                for (i = 1; i < keyframes.length - 1; i++) {
                     if (property in keyframes[i].properties) {
-                        if (progr < keyframes[i].key) {
+                        if (progr <= keyframes[i].key) {
                             secondKeyframe = keyframes[i];
                             break;
                         }
@@ -344,12 +345,14 @@
                     }
                 }
 
-                if (!firstKeyframe) firstKeyframe = keyframes[0];
-                if (!secondKeyframe) secondKeyframe = keyframes[keyframes.length - 1];
-
+                // смещение первого ключевого кадра
+                // относительно начала анимации
                 offset = firstKeyframe.key;
-                scale = 1.0 / (secondKeyframe.key - firstKeyframe.key);
-                easing = timingFunction((progr / 100 - offset) * scale);
+                // масштаб для сплющивания прогресса
+                scale = 100 * 1.0 / (secondKeyframe.key - firstKeyframe.key);
+                // приводим прогресс к долям и считаем смягчение
+                // относительно двух ключевых кадров
+                easing = timingFunction( (progr - offset) / 100 * scale );
 
                 from = normalize(element, property, firstKeyframe.properties[property]);
                 to = normalize(element, property, secondKeyframe.properties[property]);
