e10c4053af3187d5c1d6f38b583ad670fdb665fe
Colch <colouredchalkmelky@gmail.com>
2013-03-02 22:53:14 +0400

    Добавил активный класс для навигации

--- javascripts/script.js
+++ javascripts/script.js
@@ -7,10 +7,27 @@ jQuery(function () {
         container: "#content" // селектор, куда сохранится html
     };
 
+    // класс активного элемента меню
+    var activeClass = 'active';
+
+    // предыдущий активный пункт меню (селектор)
+    var previousItem;
+
+    // обработка клика по ссылке и инициализация события запроса на загрузку
     $('body').on("click", ".page-loader", loaderOptions, function (e) {
         var data = e.data;
-        var src = data.path + $(e.target).attr(data.pageAttr) + data.postFix;
+        var $target = $(e.target);
+
+        previousItem && $(previousItem).removeClass(activeClass);
+
+        !$target.attr('id') && $target.attr('id', 'loader-' + 1e4 * Math.random() | 0);
+        previousItem = $target.attr('id');
+
+        $target.addClass(activeClass);
+
+        var src = data.path + $target.attr(data.pageAttr) + data.postFix;
         $('body').trigger("requestpageload", [ e.data.container, src ]);
+
         e.preventDefault();
     });
 });
